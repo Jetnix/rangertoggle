0745fdcc4485bc5ad3ca50b48338efb2b0c35a8f
Chase Grijalva and Michael Koceja <chase@>
2017-04-25 09:09:23 -0700

    forgot to commit

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+
+$(document).ready(function() {
+  $(".clickable").click(function() {
+    $("#initial-showing").toggle();
+    $("#initial-hidden").toggle();
+  });
+});
+$(document).ready(function() {
+  $(".clickable2").click(function() {
+    $("#initial-showing2").toggle();
+    $("#initial-hidden2").toggle();
+  });
+});
+$(document).ready(function() {
+  $(".clickable3").click(function() {
+    $("#initial-showing3").toggle();
+    $("#initial-hidden3").toggle();
+  });
+});
